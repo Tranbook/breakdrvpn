a5b8871dcb26daa68faac225ceeafe79f997d47c
Tranbook <Tranbook@users.noreply.github.com>
2017-04-25 13:59:23 +0800

    Update dr.bat

--- dr.bat
+++ dr.bat
@@ -1,5 +1,5 @@
 @echo off
 for /F "tokens=3" %%* in ('route print ^| findstr "\<0.0.0.0\>"') do set "gw=%%*"
 ipconfig /flushdns
-route add 10.100.0.0 mask 255.255.255.0 %gw% metric 25
+route add IP mask 255.255.255.0 %gw% metric 25
 pause
